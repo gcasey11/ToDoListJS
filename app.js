a4f562658f50f5173dd57cf353510e6d3c413265
Gavin Ryan <grya8915@uni.sydney.edu.au>
2024-01-11 15:19:54 +1100

    Added deleting and completing todos, also added a filter for completed and incomplete

--- app.js
+++ app.js
@@ -2,9 +2,12 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo')
 
 // Event Listeners
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 // Functions
 function addTodo(event) {
@@ -32,3 +35,50 @@ function addTodo(event) {
 
     todoInput.value = "";
 }
+
+function deleteCheck(e){
+    const item = e.target;
+    //Delete Todo
+    if(item.classList[0] === 'trash-btn') {
+        const todo = item.parentElement;
+
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        });
+    } 
+
+    if (item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
+
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if(todo.classList.contains("completed")){
+                    todo.style.display = 'flex';
+                }
+                else {
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if(!todo.classList.contains("completed")){
+                    todo.style.display = 'flex';
+                }
+                else {
+                    todo.style.display = "none";
+                }
+                break;
+        }
+    });
+}
+
